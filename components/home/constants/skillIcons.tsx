6258e0db9e6518d79d23deca7f2be042928ee55a
coastellen515-ui <coastellen515@gmail.com>
2025-12-30 08:13:13 +1000

    init

---
+++ components/home/constants/skillIcons.tsx
@@ -0,0 +1,98 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiGit,
+  SiNodedotjs,
+  SiReact,
+  SiRedux,
+  SiMongodb,
+  SiSupabase,
+  SiMui,
+  SiNextdotjs,
+  SiPostgresql,
+  SiFirebase,
+  SiTailwindcss,
+  SiTypescript,
+  SiAmazonwebservices,
+  SiGooglecloud,
+  SiDocker,
+  SiPython,
+} from 'react-icons/si';
+
+const skillLogos = [
+  {
+    icon: <SiHtml5 title="Html5" />,
+    label: 'Html5',
+  },
+  {
+    icon: <SiCss3 title="CSS3" />,
+    label: 'CSS3',
+  },
+  {
+    icon: <SiJavascript title="JavaScript" />,
+    label: 'JavaScript',
+  },
+  {
+    icon: <SiTypescript title="TypeScript" />,
+    label: 'TypeScript',
+  },
+  {
+    icon: <SiGit title="Git" />,
+    label: 'Git',
+  },
+  {
+    icon: <SiReact title="React" />,
+    label: 'React',
+  },
+  {
+    icon: <SiRedux title="Redux" />,
+    label: 'Redux',
+  },
+  {
+    icon: <SiNextdotjs title="NextJS" />,
+    label: 'NextJS',
+  },
+  {
+    icon: <SiNodedotjs title="Node" />,
+    label: 'Node',
+  },
+  {
+    icon: <SiPostgresql title="PostgreSQL" />,
+    label: 'PostgreSQL',
+  },
+  {
+    icon: <SiMongodb title="MongoDB" />,
+    label: 'MongoDB',
+  },
+  {
+    icon: <SiFirebase title="Firebase" />,
+    label: 'Firebase',
+  },
+  {
+    icon: <SiSupabase title="Supabase" />,
+    label: 'Supabase',
+  },
+  {
+    icon: <SiTailwindcss title="Tailwind CSS" />,
+    label: 'Tailwind CSS',
+  },
+  {
+    icon: <SiMui title="Material UI" />,
+    label: 'Material UI',
+  },
+  {
+    icon: <SiDocker title="Docker" />,
+    label: 'Docker',
+  },
+  {
+    icon: <SiPython title="Python" />,
+    label: 'Python',
+  },
+  {
+    icon: <SiGooglecloud title="Google Cloud" />,
+    label: 'Google Cloud',
+  },
+];
+
+export default skillLogos;
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['www.freecodecamp.org'],
+  },
+};
+
+module.exports = nextConfig;
